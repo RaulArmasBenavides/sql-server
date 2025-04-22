9cfacefb592a27fb2ee2d88129ced4d92905a97f
Raul Armas <raul.armas@intelica.com>
2025-04-22 18:02:42 -0500

    update

--- 13.2.Optimizacion+I.sql
+++ 13.2.Optimizacion+I.sql
@@ -1,3 +1,6 @@
+ 
+
+
 /*1.Optimizar La busqueda A través de indices del campo NOM_PROD de la tabla tblProductos*/
 --sp_help 'tblproductos'
 
@@ -7,3 +10,21 @@ SET STATISTICS IO ON
 SELECT * FROM tblProductos tp  WHERE tp.NOM_PROD LIKE '%carne%'
 
 SET STATISTICS IO OFF
+
+
+
+
+
+select top 100 * From 
+fn_dblog (null,null) 
+
+
+SELECT XEventData.event_data.value('(event/data/value)[1]', 'varchar(max)') AS DeadlockGraph
+FROM sys.dm_xe_session_targets AS st
+JOIN sys.dm_xe_sessions AS s ON s.address = st.event_session_address
+JOIN sys.dm_xe_session_event_actions AS ea ON s.address = ea.event_session_address
+CROSS APPLY (SELECT CAST(st.target_data AS XML)) AS XEventData
+WHERE s.name = 'DeadlockMonitor';
+
+
+DBCC TRACEON (1222, -1);
--- 13.32.Optimizacion+III.sql
+++ 13.32.Optimizacion+III.sql
@@ -23,3 +23,41 @@ ON  a.object_id = Obj.object_id
 
 --ALTER INDEX PK_Grupos_COD_GRUP ON TBLGRUPOS REBUILD
 --ALTER INDEX PK_Clientes_COD_ID ON tblClientes REORGANIZE
+
+SELECT 
+  DB_NAME(db_id()) as DatabaseName,
+	    @@servername Servidor, 
+    s.name AS SchemaName,
+    t.name AS TableName,
+    i.name AS IndexName,
+    ips.index_id,
+    ips.avg_fragmentation_in_percent,
+    ips.page_count,
+	ips.fragment_count as PromPagFrag,
+    ips.index_type_desc as TipoIndice,
+    ips.avg_fragment_size_in_pages,
+    ips.partition_number
+FROM 
+    sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ips
+JOIN 
+    sys.tables AS t ON ips.object_id = t.object_id
+JOIN 
+    sys.schemas AS s ON t.schema_id = s.schema_id
+JOIN 
+    sys.indexes AS i ON ips.object_id = i.object_id AND ips.index_id = i.index_id
+WHERE 
+    s.name = 'INTCH'
+    AND t.name IN (
+	     'MPT_DOCUMENT_FILE',
+        'MPT_MDR_DISTRIBUTION',
+        'MPT_DOCUMENT_MDR',
+        'MPT_CLIENT_GROUP',
+        'ACQUIRING_SUMMARY',
+        'JURISDICTION',
+        'MPT_CLIENT_CHAIN',
+        'MPT_CLIENT_MERCHANT'
+    )
+    AND i.name IS NOT NULL
+   -- AND ips.page_count >= 100
+ORDER BY 
+    ips.avg_fragmentation_in_percent DESC;
